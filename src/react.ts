22e794e51c55da0ef38f409fcd6c926479b63f12
Massimiliano Bellan <massimilianobellan25@gmail.com>
2025-06-17 19:49:28 +0200

    working first test but not second

--- src/react.ts
+++ src/react.ts
@@ -1,12 +1,35 @@
 import { useSyncExternalStore } from "react";
-import { type Stash } from "stash";
+import { createStash, type Stash } from "stash";
 
 export function useStash<T>(stash: Stash<T>) {
-  return useSyncExternalStore(stash.subscribe, stash.getStash);
+  return useSyncExternalStore(stash.subscribe, stash.getStash, stash.getStash);
 }
 
-type CreateCallback = (set:)
+type StateCreator<T> = (
+  set: (fn: (state: T) => Partial<T>) => void,
+  get: () => T
+) => T;
 
-export function create<T>() {
+export function create<T>(initialStash: StateCreator<T>) {
+  const stash = createStash({} as T);
 
+  const get: Parameters<StateCreator<T>>[1] = function () {
+    return stash.getStash();
+  };
+
+  const set: Parameters<StateCreator<T>>[0] = function (fn) {
+    const next = fn(stash.getStash());
+    stash.setStash((prev) => ({ ...prev, ...next }));
+  };
+
+  const initialState = initialStash(set, get);
+  stash.setStash(initialState);
+
+  function useBoundStash() {
+    return useStash(stash);
+  }
+
+  Object.assign(useBoundStash, stash);
+
+  return useBoundStash;
 }
--- test/basic.test.tsx
+++ test/basic.test.tsx
@@ -1,17 +1,30 @@
 import { test, expect } from "vitest";
-import { createStash, useStash } from "stash";
+import { create } from "stash";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { useState } from "react";
 
-test("it works", async () => {
-  const createdStash = createStash({ count: 0, setCount: () => count + 1 });
+test("can create a store", async () => {
+  type MyStash = {
+    count: number;
+    incrementCount: () => void;
+  };
+
   function Component() {
-    const stash = useStash(createdStash);
+    const [useStash] = useState(() =>
+      create<MyStash>((set) => ({
+        count: 0,
+        incrementCount: () => {
+          set(({ count }) => ({ count: count + 1 }));
+        },
+      }))
+    );
+    const { count, incrementCount } = useStash();
 
     return (
       <>
-        <div>{stash.count}</div>
-        <button>Click</button>
+        <div>{count}</div>
+        <button onClick={incrementCount}>Click</button>
       </>
     );
   }
@@ -20,5 +33,59 @@ test("it works", async () => {
 
   await userEvent.click(screen.getByText("Click"));
 
-  expect(screen.findByText("1")).toBeInTheDocument();
+  expect(screen.getByText("1")).toBeInTheDocument();
+});
+
+test("all components rerender", async () => {
+  type MyStash = {
+    count: number;
+    incrementCount: () => void;
+  };
+
+  function ParentComponent() {
+    const [useStash] = useState(() =>
+      create<MyStash>((set) => ({
+        count: 0,
+        incrementCount: () => {
+          set(({ count }) => ({ count: count + 1 }));
+        },
+      }))
+    );
+
+    return (
+      <>
+        <Component1 useStash={useStash} />
+        <Component2 useStash={useStash} />
+      </>
+    );
+  }
+
+  function Component1({ useStash }: { useStash: () => MyStash }) {
+    const { count, incrementCount } = useStash();
+
+    return (
+      <>
+        <div>{count}</div>
+        <button onClick={incrementCount}>Click</button>
+      </>
+    );
+  }
+
+  function Component2({ useStash }: { useStash: () => MyStash }) {
+    const { count } = useStash();
+
+    return (
+      <>
+        <div>{count}</div>
+      </>
+    );
+  }
+
+  render(<ParentComponent />);
+
+  expect(screen.getAllByText("1")).toHaveLength(0);
+
+  await userEvent.click(screen.getByText("Click"));
+
+  expect(screen.getAllByText("1")).toHaveLength(2);
 });
